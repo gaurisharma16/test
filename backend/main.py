e8ea34c4e22d60866bce0ac84bdce1ad13ac4254
Gauri Sharma <gaurisharmabts@gmail.com>
2026-01-02 03:37:23 +0530

    third commit

--- backend/main.py
+++ backend/main.py
@@ -25,6 +25,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:8080",
         "http://127.0.0.1:5173",
+        "https://finexa-ai.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
